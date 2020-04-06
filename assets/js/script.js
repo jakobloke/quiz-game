c0eb72b3dffeb0c2de65807438aab73bc98f217d
jakobloke <61665831+jakobloke@users.noreply.github.com>
2020-04-05 23:50:30 -0700

    added timer and button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,13 @@ var questionElement = document.getElementById('question');
 var answerButtonsElement = document.getElementById('answer-buttons');
 var questionElement = document.getElementById('question');
 var bannerElement = document.getElementById('banner');
+var timeClockElement = document.getElementById('timer');
+var quizTimer, display, score;
 
-let shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex;
 
 var count = 0;
+
 var questions = [
     {
         question: "Commonly used data types DO Not Include:",
@@ -56,38 +59,93 @@ var questions = [
     }
 ];
 
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', startGame);
+answerButtonsElement.addEventListener('click', () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
 
 function startGame() {
-    startButton.classList.add('hide')
-    answerButtonsElement.classList.remove('hide')
-    bannerElement.classList.add('hide')
-    currentQuestionIndex = 0
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    setNextQuestion()
-
+    startButton.classList.add('hide');
+    //answerButtonsElement.classList.remove('hide')
+    bannerElement.classList.add('hide');
+    currentQuestionIndex = 0;
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    setNextQuestion();
 }
-
 
 function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+    count++
+    if (count > 4) {
+        clearInterval();
+        score = timer.innerText;
+    }
+    while(answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.lastChild)
+    };
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
 
-}
 function showQuestion(question) {
-    questionElement.innerText = question.question
+    questionElement.innerText = question.question;
     question.answer.forEach(answer => {
-        var button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-}
+        var button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
+};
 
 function selectAnswer(e) {
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.correct
+    var selectedButton = e.target;
+    var correct = selectedButton.dataset.correct;
+    timeBank(selectedButton);
+};
+
+function timeBank(selected) {
+     var answerCheck;
+    for(i = 0; i < questions.length; i++){
+        if(questionElement.innerText == questions[i].question){{
+            for(j = 0; j < questions[i].answer.length; j++){
+                if(selected.innerText == questions[i].answer[j].text){
+                    answerCheck = questions[i].answer[j].correct;
+                }
+             }
+        }}
+    }
+//     if(answerCheck == false) {
+        
+// };
+}
+
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+      let  minutes = parseInt(timer / 60, 10);
+      let  seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer == 0) {
+            stopQuiz();
+        }
+    }, 1000);
+};
+
+window.onload = function () {
+    quizTimer = 60 * 1.25,
+    display = timeClockElement;
+    startTimer(quizTimer, display);
+};
+
+function stopQuiz () {    
+    document.getElementById('timer').style.display = "none";
+    document.getElementById('question container').style.display = "none";
+    document.getElementById('start-btn').style.display = "none";
+    document.getElementById('myText').style.display = "";
+    window.open("highscores.html?timer="+score+"&name="+myText.innerText);
 }
